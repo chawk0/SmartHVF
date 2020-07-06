bfddc925b021577c4b00165581699236c1b8e0f3
unknown <chawkzero@gmail.com>
2020-07-06 17:04:28 -0400

    test commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject quad;
+    private bool moveQuad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveQuad = false;
+        //quad = GameObject.Find("Quad2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            moveQuad = true;
+        else if (Input.GetKeyUp(KeyCode.Space))
+            moveQuad = false;
+
+        if (moveQuad)
+            quad.transform.Translate(0.01f, 0, 0);
+    }
+}
